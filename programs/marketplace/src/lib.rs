c2b8f6b9d09809df69e391ea965fd2eab581854e
Ayush <ayushagarwal.dev@gmail.com>
2024-11-07 10:49:15 +0530

    initial commit

---
+++ programs/marketplace/src/lib.rs
@@ -0,0 +1,19 @@
+use anchor_lang::prelude::*;
+
+declare_id!("ECyPjsZBXBAqqAMEeDPPJEkUHGfxMU54d9LpGm5MRAFy");
+
+mod state;
+mod instructions;
+
+#[program]
+pub mod marketplace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
